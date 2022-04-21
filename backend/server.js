48ffb64832d443144b6d130bf7fea050a5a68de7
B00107768 Luke Tormey <b00107768@mytudublin.ie>
2022-04-21 12:59:56 +0100

    Route and controller made for goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
