4ddc0b6b5467a5f766b24dcf29db1c20f291ddfc
B00107768 Luke Tormey <b00107768@mytudublin.ie>
2022-04-22 15:04:43 +0100

    Security implemented with bcrpyt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,7 +72,12 @@ const loginUser = asyncHandler(async(req, res) => {
 // @access  Private
 
 const getCurrentUser = asyncHandler(async(req, res) => {
-    res.json({message: 'Yo dawg'})
+    const { _id, name, email } = await User.findById(req.user.id)
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 })
 
 // Generate JWT
